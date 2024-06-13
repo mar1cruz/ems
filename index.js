6e0e2f37caee53e0112d9428bc7eed7e033eca7f
dmitry <ddovidovich@list.ru>
2024-06-13 21:07:37 +0300

    add overlay

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const btnTryForFree = document.querySelector(".button-link");
 const closePopupBtn = document.querySelector(".close-btn");
 const popup = document.querySelector(".popup");
+const overlay = document.querySelector(".overlay");
 
 window.addEventListener("click", (e) => {
   if (!popup.contains(e.target) && !btnTryForFree.contains(e.target)) {
     popup.classList.remove("active");
+    overlay.classList.remove("active");
   }
 });
 
 btnTryForFree.addEventListener("click", (e) => {
   e.stopPropagation();
   popup.classList.add("active");
+  overlay.classList.add("active");
 });
 
 closePopupBtn.addEventListener("click", (e) => {
   e.stopPropagation();
   popup.classList.remove("active");
+  overlay.classList.remove("active");
 });
